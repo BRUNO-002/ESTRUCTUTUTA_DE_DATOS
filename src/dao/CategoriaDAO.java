403ebd99daef2c2929b417ed98b6fe36b45a7098
BRUNO-002 <0333242004@unjfsc.edu.pe>
2026-05-03 18:57:41 -0500

    Agreando en CategoriaDao

--- src/dao/CategoriaDAO.java
+++ src/dao/CategoriaDAO.java
@@ -2,7 +2,7 @@
 package dao;
 import dto.Categoria;
 public class CategoriaDAO {
-    private Categoria raiz;
+    private Categoria raiz;//raiz es igual cabeza
     private Categoria ultimo;
     
     public void agregar(Categoria p){
@@ -21,5 +21,49 @@ public class CategoriaDAO {
             p=p.getSig();
         }
         return listado;
+    }
+    public void agregarAlInicio(Categoria p){
+        if(raiz==null){
+            raiz=ultimo=null;
+        }else{
+            p.setSig(raiz);
+            raiz=p;
+        }
+    }
+    //burble short
+    public void ordenarPorCodigo(){
+        if(raiz==null || raiz.getSig()==null){
+            return;
+        }
+        boolean Intercambio;
+        do {            
+            Intercambio=false;
+            Categoria actual=raiz;
+            Categoria anterior=null;
+            Categoria siguiente=raiz.getSig();
+            while(actual.getSig()!=null){
+                if(actual.getCodiCate()>actual.getSig().getCodiCate()){
+                    actual.setSig(siguiente.getSig());
+                    siguiente.setSig(actual);
+                    if (anterior==null) {
+                        raiz=siguiente;
+                    }else{
+                        anterior.setSig(siguiente);
+                    }
+                    anterior=siguiente;
+                    siguiente=actual.getSig();
+                    Intercambio=true;
+                }else{
+                    anterior=actual;
+                    actual=siguiente;
+                    siguiente=siguiente.getSig();
+                }
+            }
+        } while (Intercambio);
+        Categoria temp=raiz;
+        while(temp!=null && temp.getSig()!=null){
+            temp=temp.getSig();
+        }
+        ultimo=temp;
     }
 }

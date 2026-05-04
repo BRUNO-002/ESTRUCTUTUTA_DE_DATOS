bacc421b7449608442b3eaebfaf84756db4fa43f
CamilaFallaArrieta <0333242008@unjfsc.edu.pe>
2026-05-03 23:10:36 -0500

    Update

--- src/com/mycompany/ed0005/ED0005.java
+++ src/com/mycompany/ed0005/ED0005.java
@@ -10,6 +10,7 @@ class ED0005 {
         Lista l=new Lista();
         l.agregar(new Nodo(3, null));
         l.agregar(new Nodo(5, null));
+        l.agregar(new Nodo(5, null));
         l.agregar(new Nodo(7, null));
         l.agregar(new Nodo(9, null));
         l.agregar(new Nodo(90, null));
